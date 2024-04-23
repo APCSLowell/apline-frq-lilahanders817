9c7168fe7afa5a10978591b3ccb0112a246ed8f3
lilahanders817 <143014915+lilahanders817@users.noreply.github.com>
2024-04-23 04:44:41 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,17 @@
-public class APLine
-{
-  /* your code here */
-  
+public class APLine{
+  private int myA, myB, myC;
+  public APLine(int a, int b, int c){
+    myA = a;
+    myB = b;
+    myC = c;
+  }
+  public double getSlope(){
+    return(-(myA)/(double)(myB));
+  }
+  public boolean isOnline(int x, int y){
+    if(((myA*x) + (myB*y) - myC) == 0){
+      return true;
+    }
+    return false;
+  }
 }
